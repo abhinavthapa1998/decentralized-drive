428680e857fdcbf4359a1a8774af4a4afbafd15e
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-01-24 23:14:27 +0530

    Progress

--- contracts/Upload.sol
+++ contracts/Upload.sol
@@ -10,4 +10,30 @@ contract Upload {
     mapping(address => string[]) value;
     mapping(address => mapping(address => bool)) ownership;
     mapping(address => Access[]) accessList;
+    mapping(address => mapping(address => bool)) previousData;
+
+    function add(address _user, string memory url) external {
+        value[_user].push(url);
+    }
+
+    function allow(address user) external {
+        ownership[msg.sender][user] = true;
+        accessList[msg.sender].push(Access(user, true));
+        if (previousData[msg.sender][user]) {
+            for (uint256 i = 0; i < accessList[msg.sender].length; i++) {
+                if (accessList[msg.sender][i].user == user) {
+                    accessList[msg.sender].push(Access(user, true));
+                }
+            }
+        }
+    }
+
+    function disallow(address user) public {
+        ownership[msg.sender][user] = false;
+        for (uint256 i = 0; i < accessList[msg.sender].length; i++) {
+            if (accessList[msg.sender][i].user == user) {
+                accessList[msg.sender][i].access = false;
+            }
+        }
+    }
 }
